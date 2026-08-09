bb9ac925a25fcbe5b99b0c6c86de5f3ba4293676
code-shubhambhatt <shubhambhatt1000@gmail.com>
2026-08-09 19:06:11 +0530

    refactor: move API base URL to environment variable

--- frontend/src/api/about.js
+++ frontend/src/api/about.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api"
+const API_BASE = import.meta.env.VITE_API_BASE
 
 export async function updateAbout(about){
     const token = localStorage.getItem("access_token")
